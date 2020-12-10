1a4c42121a6bc51fcb87986413246d64a8e0c747
mrchoi87 <mrchoi8708@gmail.com>
2020-12-10 02:10:07 -0500

    remove ignored file

--- ui/node_modules/vue-loop/Examples/assets/js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-window._ = require('lodash');
-window.Vue = require('vue');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to a back-end.
- */
-window.axios = require('axios');
--- ui/node_modules/vue-loop/Examples/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('assets/js/app.js', 'public/js/')
-   .sass('assets/scss/app.scss', 'public/css/');
